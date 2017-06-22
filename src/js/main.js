b2f2add21d9f75a6bbecd2ae87f1ace5e0f37e91
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-06-21 18:00:18 -0700

    updates: working on fixing highlighting issue on dots

--- src/js/main.js
+++ src/js/main.js
@@ -14,19 +14,22 @@ var listItem = $(".list-item");
 // var videoTitle = $.one(".title");
 
 var displayVideo = function(e) {
-// console.log(this);
-
+console.log(this);
 lightbox.style.display = "block";
 var youtube = this.getAttribute("data-youtube");
 listItem.forEach(el => el.classList.remove("active")); //removes highlight from list item
 this.classList.add("active"); //highlights list item
 video.setAttribute("src", `https://www.youtube.com/embed/${youtube}?ecver=1&autoplay=1&showinfo=0&rel=0`);
+
+//for dots
+var id = this.id;
+
 };
 
 var closeWindow = function(e) {
 lightbox.style.display = "none";
 video.setAttribute("src", "");
-this.classList.remove("active");
+listItem.forEach(el => el.classList.remove("active")); //removes highlight from list item
 };
 
 points.forEach(l => l.addEventListener("click", displayVideo));
